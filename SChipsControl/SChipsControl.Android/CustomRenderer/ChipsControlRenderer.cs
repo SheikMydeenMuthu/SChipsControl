60f0fdf95655a1dc1e0f4035e4d440b12f9d7290
Sheik Muthu <Sheik.Muthu@sisystems.com>
2020-05-04 18:58:10 +0530

    Add project files.

---
+++ SChipsControl/SChipsControl.Android/CustomRenderer/ChipsControlRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SChipsControl.CustomControls;
+using SChipsControl.Droid.CustomRenderer;
+using Xamarin.Forms;
+
+[assembly:ExportRenderer(typeof(ChipsControl),typeof(ChipsControlRenderer))]
+namespace SChipsControl.Droid.CustomRenderer
+{
+    public class ChipsControlRenderer//:FlexLayoutRenderer
+    {
+    }
+}
---
+++ SChipsControl/SChipsControl/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SChipsControl.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
